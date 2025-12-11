40a6e9ae3f23b5080b8abc5b5247429338016794
Vaaris Chitkara <vaarischitkara16@gmail.com>
2025-12-11 22:21:55 +0530

    os variable mixup fix

--- lua/utils/make_file.lua
+++ lua/utils/make_file.lua
@@ -1,14 +1,14 @@
 local M = {}
 
-function M.make_file(os, pathFile, OSes)
+function M.make_file(osType, pathFile, OSes)
     if vim.fn.filereadable(pathFile) == 1 then
         print("notes.txt exists")
         return
     else
 
-        if os == OSes[1] then
+        if osType == OSes[1] then
             os.execute('type NUL > "' .. pathFile .. '"')
-        elseif os == OSes[2] or os == OSes[3] then
+        elseif osType == OSes[2] or osType == OSes[3] then
             os.execute('touch "' .. pathFile ..'"')
         end
     end
--- lua/utils/make_folder.lua
+++ lua/utils/make_folder.lua
@@ -1,14 +1,14 @@
 local M = {}
 
-function M.make_folder(os, OSes, pathFolder)
+function M.make_folder(osType, OSes, pathFolder)
 
-    if os == OSes[1] then
+    if osType == OSes[1] then
         os.execute('mkdir "' .. pathFolder .. '"')
         print("OS: Windows, QuickNote made at" .. pathFolder)
-    elseif os == OSes[2] or os == OSes[3] then
+    elseif osType == OSes[2] or osType == OSes[3] then
         os.execute('mkdir -p "' .. pathFolder .. '"')
         print("OS: Linx/Macos, QucikNote made at" .. pathFolder)
     end
 end
 
-return M
+return M 
